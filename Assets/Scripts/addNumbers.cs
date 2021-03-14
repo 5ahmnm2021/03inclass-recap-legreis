825522987cdaa9bb29112794a3b10b2ad61ea2b8
leonigreisberger <leoni.greisberger@gmail.com>
2021-03-14 16:37:14 +0100

    AddNumber

---
+++ Assets/Scripts/addNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class addNumbers : MonoBehaviour
+{
+    public InputField Num01;
+    public InputField Num02;
+    public Text Result;
+
+    public void AddNumbersOnClick()
+    {
+        float Num1 = float.Parse(Num01.text);
+        float Num2 = float.Parse(Num02.text);
+        float addResult = Num1 + Num2;
+        Result.text = addResult.ToString();
+    }
+}
