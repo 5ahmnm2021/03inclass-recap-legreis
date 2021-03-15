6c541f364014fcf350685a642fbd485e49b902ec
leonigreisberger <leoni.greisberger@gmail.com>
2021-03-15 20:45:11 +0100

    homework

--- Assets/Scripts/addNumbers.cs
+++ Assets/Scripts/addNumbers.cs
@@ -8,12 +8,35 @@ public class addNumbers : MonoBehaviour
     public InputField Num01;
     public InputField Num02;
     public Text Result;
+    public Text Error;
+
+    public float num1;
+    public float num2;
 
     public void AddNumbersOnClick()
     {
-        float Num1 = float.Parse(Num01.text);
-        float Num2 = float.Parse(Num02.text);
-        float addResult = Num1 + Num2;
-        Result.text = addResult.ToString();
+        try
+        {
+            num1 = float.Parse(Num01.text);
+        }
+        catch (System.Exception)
+        {
+            Num01.image.color = new Color32(225, 0, 0, 100);
+            Error.text = "Gib eine Zahl ein!";
+        }
+        try
+        {
+            num2 = float.Parse(Num02.text);
+        }
+        catch (System.Exception)
+        {
+            Num02.image.color = new Color32(255, 0, 0, 100);
+            Error.text = "Gib eine Zahl ein!";
+        }
+
+        //float addResult = num1 + num2;
+        Result.text = (num1 + num2).ToString();
+        Debug.Log("Das Ergebnis ist: " + Result.text);
+
     }
 }
